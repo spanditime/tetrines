e64be9443e1f6e2f29093c06e6998560fa78a704
spanditime <spanditime@gmail.com>
2019-07-13 17:52:28 +0700

    add GuiElement class declaration

---
+++ GUI/GuiElement.hpp
@@ -0,0 +1,50 @@
+#ifndef GUI_GUIELEMENT_HPP
+#define GUI_GUIELEMENT_HPP
+
+#include <SFML/Graphics/Drawable.hpp>
+
+class GUI;
+
+class GuiElement{
+public:
+    void setPosX(float value, bool in_percent = false);
+    void setPosY(float value, bool in_percent = false);
+    void setWidth(float value, bool in_percent = false);
+    void setHeight(float value, bool in_percent = false);
+    bool isFocused();
+
+private:
+    friend class GUI;
+    void _draw(const sf::FloatRect& p,sf::RenderTarget& target, sf::RenderStates states) const;
+    virtual void draw(const sf::FloatRect& p,sf::RenderTarget& target, sf::RenderStates states) const = 0;
+    virtual void update(float collapsed_time)=0;
+    
+    virtual void onHover();
+    virtual void onMouseLeave();
+    virtual bool mouseHovered();
+    virtual void mouseMoved(const sf::FloatRect& r,float x, float y);
+    virtual void mousePressed(const sf::FloatRect& r,float x, float y,sf::Mouse::Button b);
+    virtual bool mouseReleased(const sf::FloatRect& r,float x, float y,sf::Mouse::Button b);
+
+    virtual bool canHaveFocus() const;
+    virtual void onFocus();
+    virtual void lostFocus();
+    virtual void onReturn();
+    virtual bool leftArrow();
+    virtual bool rightArrow();
+    virtual void plus();
+    virtual void minus();
+
+    bool focused;
+    bool hover;
+    bool mouse_pressed;
+
+    GUI *parrent;
+
+    sf::FloatRect getRect(const sf::FloatRect& p) const;
+
+    sf::FloatRect rect;
+    sf::Rect<bool> in_perc;
+};
+
+#endif // !GUI_GUIELEMENT_HPP
