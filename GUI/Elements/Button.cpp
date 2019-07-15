fd006dd299d3ac8dc1ba7be83b87eb5dca247aca
spanditime <spanditime@gmail.com>
2019-07-15 16:13:28 +0700

    GuiElement Button init commit

---
+++ GUI/Elements/Button.cpp
@@ -0,0 +1,152 @@
+#include "Button.hpp"
+
+#include <cmath>
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+
+void Button::default_button_action(){}
+
+Button::Button(){
+    button_action = default_button_action;
+    circle_size = 0;
+    circle = {0,0};
+}
+
+void Button::setAction(void(*action)()){
+    if(action != nullptr)
+        button_action = action;
+    else if(button_action == nullptr)
+        button_action = default_button_action;
+}
+
+void Button::setBackgroundColor(sf::Color color){
+    background_color = color;
+}
+
+void Button::setFillColor(sf::Color color){
+    fill_color = color;
+}
+
+void Button::setText(std::string str){
+    
+}
+
+float evaluatLength(float x,float y){
+    return x+y;
+}
+
+void Button::draw(sf::RenderTarget& target, sf::RenderStates states) const{
+    sf::FloatRect r =getRect();
+    sf::RectangleShape rect;
+    rect.setPosition(sf::Vector2f(r.left,r.top));
+    rect.setSize(sf::Vector2f(r.width,r.height));
+    rect.setFillColor(background_color);
+    sf::RenderTexture t;
+    if(circle_size>0&&circle.x>=0&&circle.x<=1&&circle.y>=0&&circle.y<=1){
+        t.create((int)(r.width+0.5f),(int)(r.height+0.5f));
+        float R=evaluatLength(r.width,r.height)/2.f*circle_size;
+        sf::CircleShape shape(R,60L);
+        shape.setPosition(r.width*circle.x-R,r.height*(1-circle.y)-R);
+        shape.setFillColor(fill_color);
+        t.setSmooth(true);
+        t.clear(background_color);
+        t.draw(shape);
+        rect.setTexture(&t.getTexture());
+    }
+    target.draw(rect);
+}
+
+void Button::update(float collapsed_time){
+    if((isMousePressed()||entered)&&circle_size>=0.5f){
+        circle_size = std::sqrt((circle_size-0.5f)/1.5f);
+        circle_size += (collapsed_time*2.f);
+        circle_size *= circle_size * 1.5f;
+        circle_size += 0.5f;
+        if(circle_size>=2.f){
+            button_action();
+            entered = false;
+        }
+    }else if((isFocused()||mouseHovered()||isMousePressed()||entered)&&circle_size<0.5f){
+        circle_size = std::sqrt(circle_size*2)+collapsed_time*2;
+        if(circle_size>=1.f){
+            circle_size = 0.5f;
+        }else{
+            circle_size *= circle_size / 2.f;
+        }
+    }else if((!(isMousePressed()||entered))&&circle_size>0.5f){
+        circle_size = std::sqrt((circle_size-0.5f)/1.5f);
+        circle_size -= (collapsed_time*4.f);
+        if(circle_size<=0)
+            circle_size = 0.5f;
+        else{
+            circle_size *= circle_size * 1.5f;
+            circle_size += 0.5f;
+        }
+    } else if((!(isFocused()||mouseHovered()||isMousePressed()||entered))&&circle_size>0.0f){
+        circle_size = std::sqrt(circle_size*2)-collapsed_time*4;
+        if(circle_size<=0.f){
+            circle_size = 0.0f;
+            circle.x = 0.5f;
+            circle.y = 0.5f;
+        }else{
+            circle_size *= circle_size / 2.f;
+        }
+    }
+}
+
+void Button::onHover(){
+
+}
+
+void Button::onMouseLeave(){
+
+}
+
+void Button::mouseMoved(float x, float y){
+    sf::FloatRect r = getRect();
+    circle.x = (x - r.left) / r.width;
+    circle.y = (y - r.top) / r.height;
+}
+
+bool Button::mousePressed(float x, float y,sf::Mouse::Button b){
+    sf::FloatRect r = getRect();
+    circle.x = (x - r.left) / r.width;
+    circle.y = (y - r.top) / r.height;
+    if(b == sf::Mouse::Left){
+        std::cout<<"pressed"<<circle_size<<"\n";
+        entered = true;
+        return true;
+    }
+    return false;
+}
+
+bool Button::mouseReleased(float x, float y,sf::Mouse::Button b){
+    sf::FloatRect r = getRect();
+    if(r.left>=x&&r.left+r.width<=x&&r.top>=y&&r.top+r.height<=y){
+        circle.x = (x - r.left) / r.width;
+        circle.y = (y - r.top) / r.height;
+    }
+    if(b == sf::Mouse::Left)
+        return false;
+    return true;
+}
+
+bool Button::canHaveFocus() const{
+    return true;
+}
+
+void Button::onFocus(){
+    if(!mouseHovered()){
+        circle.x = 0.5f;
+        circle.y = 0.5f;
+    }
+}
+
+void Button::lostFocus(){
+
+}
+
+void Button::onReturn(){
+    entered = true;
+}
---
+++ GUI/Elements/Button.hpp
@@ -0,0 +1,43 @@
+#ifndef GUI_ELEMENTS_BUTTON_HPP
+#define GUI_ELEMENTS_BUTTON_HPP
+
+#include <SFML/Graphics/Color.hpp>
+#include <string>
+
+#include "GUI/GuiElement.hpp"
+
+
+class Button : public GuiElement{
+public:
+    Button();
+    
+    static void default_button_action();
+
+    void setAction(void(*action)() = default_button_action);
+    void setBackgroundColor(sf::Color color);
+    void setFillColor(sf::Color color);
+    void setText(std::string str);
+
+private:
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const;
+    void update(float collapsed_time);
+
+    void onHover();
+    void onMouseLeave();
+    void mouseMoved(float x, float y);
+    bool mousePressed(float x, float y,sf::Mouse::Button b);
+    bool mouseReleased(float x, float y,sf::Mouse::Button b);
+
+    bool canHaveFocus() const;
+    void onFocus();
+    void lostFocus();
+    void onReturn();
+
+    sf::Color background_color, fill_color;
+    sf::Vector2f circle;
+    float circle_size;
+    bool entered;
+    void (*button_action)();
+};
+
+#endif // !GUI_ELEMENTS_BUTTON_HPP
