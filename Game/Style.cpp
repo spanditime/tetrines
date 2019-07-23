bc9c61a4fbb208f624a9802411472a5233411864
spanditime <spanditime@gmail.com>
2019-07-23 21:05:05 +0700

    add resize handler for 16:9 ratio gui

--- Game/Style.cpp
+++ Game/Style.cpp
@@ -1,10 +1,26 @@
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Rect.hpp>
 
 namespace Style{
 
-const sf::Color background_button_color=sf::Color();
-const sf::Color fill_button_color=sf::Color();
-const sf::Color background_color=sf::Color();
+    const sf::Color background_button_color=sf::Color();
+    const sf::Color fill_button_color=sf::Color();
+    const sf::Color background_color=sf::Color();
 
-
-}
+    sf::FloatRect resizeHandler16_9(float w,float h){
+        float ratio = 16.f/9.f;
+        sf::FloatRect r;
+        if(ratio<=(w/h)){
+            r.height = h;
+            r.top = 0.f;
+            r.width = h * ratio;
+            r.left = (w - r.width)/2.f;
+        }else{
+            r.width = w;
+            r.left = 0.f;
+            r.height = w / ratio;
+            r.top = (h - r.height)/2.f;
+        }
+        return r;
+    }
+}   
--- Game/Style.hpp
+++ Game/Style.hpp
@@ -2,11 +2,14 @@
 #define STYLE_HPP
 
 #include <SFML/Graphics/Color.hpp>
+#include <SFML/Graphics/Rect.hpp>
 
 namespace Style{
     extern const sf::Color background_button_color;
     extern const sf::Color fill_button_color;
     extern const sf::Color background_color;
+
+    sf::FloatRect resizeHandler16_9(float w,float h);
 }
 
 #endif // !STYLE_HPP
